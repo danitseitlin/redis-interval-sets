fb86f7778b8ef01c94b4b5797958c1db9d0a937c
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-02-23 22:53:29 +0200

    Adding is_add & is_get

--- src/lib.rs
+++ src/lib.rs
@@ -2,73 +2,147 @@
 extern crate redis_module;
 
 use redis_module::native_types::RedisType;
-use redis_module::raw::RedisModuleTypeMethods;
-use redis_module::{raw as rawmod, NextArg};
-use redis_module::{Context, RedisError, RedisResult, RedisValue, REDIS_OK};
-use serde_json::{Number, Value};
+use redis_module::{raw, Context, NextArg, RedisResult};
+use std::os::raw::c_void;
 
-use std::os::raw::c_int;
-use std::{i64, usize};
+#[derive(Debug)]
+struct IntervalSetType {
+    sets: SetType[]
+}
 
-mod array_index;
-mod backward;
-mod commands;
-mod error;
-mod formatter;
-mod nodevisitor;
-mod redisjson;
-mod schema; // TODO: Remove
+struct SetType {
+    member: String,
+    min_score: Number,
+    max_score: Number
+}
 
-use crate::array_index::ArrayIndex;
-use crate::commands::index;
-use crate::error::Error;
-
-pub const VERSION: i32 = 1;
-
-static RIS_TYPE: RedisType = RedisType::new(
-    "RedisIntervalSets",
-    VERSION,
-    RedisModuleTypeMethods {
-        version: redis_module::TYPE_METHOD_VERSION,
-
-        rdb_load: Some(redisjson::type_methods::rdb_load),
-        rdb_save: Some(redisjson::type_methods::rdb_save),
-        aof_rewrite: None, // TODO add support
-        free: Some(redisjson::type_methods::free),
+static MY_REDIS_TYPE: RedisType = RedisType::new(
+    "IntervalSetType",
+    0,
+    raw::RedisModuleTypeMethods {
+        version: raw::REDISMODULE_TYPE_METHOD_VERSION as u64,
+        rdb_load: None,
+        rdb_save: None,
+        aof_rewrite: None,
+        free: Some(free),
 
         // Currently unused by Redis
         mem_usage: None,
         digest: None,
 
-        // Auxiliary data (v2)
-        aux_load: Some(redisjson::type_methods::aux_load),
-        aux_save: Some(redisjson::type_methods::aux_save),
-        aux_save_triggers: rawmod::Aux::Before as i32,
+        // Aux data
+        aux_load: None,
+        aux_save: None,
+        aux_save_triggers: 0,
     },
 );
 
-fn is_add() 
+unsafe extern "C" fn free(value: *mut c_void) {
+    Box::from_raw(value as *mut IntervalSetType);
+}
 
-fn is_del()
 
-fn is_filter()
+fn is_add(ctx: &Context, args: Vec<String>) -> RedisResult {
+    let mut args = args.into_iter().skip(1);
+    let key = args.next_string()?;
+    let member = args.next_string()?;
+    let min_score = args.next_i64()?;
+    let max_score = args.next_i64()?;
+
+    let key = ctx.open_key_writable(&key);
+    
+    match key.get_value::<IntervalSetType>(&IntervalSetType)? {
+        Some(value) => {
+            value.sets.push({
+                member: member,
+                min_score: min_score,
+                max_score: max_score
+            })
+            key.set_value(&IntervalSetType, value)?;
+        }
+        None => {
+            let value = IntervalSetType {
+                sets: {
+                    member: member,
+                    min_score: min_score,
+                    max_score: max_score
+                }
+            };
+
+            key.set_value(&IntervalSetType, value)?;
+        }
+    }
+
+    Ok(size.into())
+}
+
+fn is_get(ctx: &Context, args: Vec<String>) -> RedisResult {
+    let mut args = args.into_iter().skip(1);
+    let key = args.next_string()?;
+
+    let key = ctx.open_key(&key);
+
+    let value = match key.get_value::<IntervalSetType>(&IntervalSetType)? {
+        Some(value) => value.data.as_str().into(),
+        None => ().into(),
+    };
+
+    Ok(value)
+}
+
+/*
+fn alloc_set(ctx: &Context, args: Vec<String>) -> RedisResult {
+    let mut args = args.into_iter().skip(1);
+    let key = args.next_string()?;
+    let size = args.next_i64()?;
+
+    ctx.log_debug(format!("key: {}, size: {}", key, size).as_str());
+
+    let key = ctx.open_key_writable(&key);
+
+    match key.get_value::<MY_REDIS_TYPE>(&MY_REDIS_TYPE)? {
+        Some(value) => {
+            value.data = "B".repeat(size as usize);
+        }
+        None => {
+            let value = IntervalSetType {
+                data: "A".repeat(size as usize),
+            };
+
+            key.set_value(&MY_REDIS_TYPE, value)?;
+        }
+    }
+
+    Ok(size.into())
+}
+
+fn alloc_get(ctx: &Context, args: Vec<String>) -> RedisResult {
+    let mut args = args.into_iter().skip(1);
+    let key = args.next_string()?;
+
+    let key = ctx.open_key(&key);
+
+    let value = match key.get_value::<IntervalSetType>(&MY_REDIS_TYPE)? {
+        Some(value) => value.data.as_str().into(),
+        None => ().into(),
+    };
+
+    Ok(value)
+}*/
 
 //////////////////////////////////////////////////////
 
-pub extern "C" fn init(raw_ctx: *mut rawmod::RedisModuleCtx) -> c_int {
-    crate::commands::index::schema_map::init();
-}
-
 redis_module! {
-    name: "RedisIntervalSets",
-    version: 99_99_99,
+    name: "interval_set",
+    version: 1,
     data_types: [
-        RIS_TYPE,
+        IntervalSetType,
+        SetType
     ],
-    init: init,
     commands: [
-        ["is.add", is_add, "write", 1,1,1],
-        ["is.filter", is_filter, "readonly", 1,1,1],
-        ["is.del", is_del, "write", 1,1,1]
+        ["is.add", is_add, "write", 1, 1, 1],
+        ["is.get", is_get, "readonly", 1, 1, 1]
+        //["alloc.set", alloc_set, "write", 1, 1, 1],
+        //["alloc.get", alloc_get, "readonly", 1, 1, 1],
     ],
 }
